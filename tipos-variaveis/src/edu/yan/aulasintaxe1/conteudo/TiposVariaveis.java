24c9743dc459367bae8fcc38545baf825724e198
Yan-Balbi <yanbalbi@gmail.com>
2025-03-17 16:36:22 -0300

    Tipos primitivos e casts

--- tipos-variaveis/src/edu/yan/aulasintaxe1/conteudo/TiposVariaveis.java
+++ tipos-variaveis/src/edu/yan/aulasintaxe1/conteudo/TiposVariaveis.java
@@ -2,6 +2,14 @@ package edu.yan.aulasintaxe1.conteudo;
 
 public class TiposVariaveis {
 	public static void main(String[] args) {
+		/*tipos primitivos*/
+		double salarioMinimo = 1700;
+		float pi = 3.14F;
+		double salarioBruto = 1650.36;
 		
+			//usando casts
+		short numeroCurto1 = 1;
+		int numeroNormal = numeroCurto1;
+		short numeroCurto2 = (short) numeroNormal;
 	}
 }
