f616fa155731c1880dd655076b792602034ae9bb
Yan-Balbi <yanbalbi@gmail.com>
2025-03-19 13:49:27 -0300

    Conteúdo de aula de sintaxe.

---
+++ java-documentation/src/edu/yan/aulasintaxe/exemplodocumentacao/Calculadora.java
@@ -0,0 +1,21 @@
+package edu.yan.aulasintaxe.exemplodocumentacao;
+
+/**@author yanba
+ * @version 1.0
+ * @since 18/03/2025
+ */
+public class Calculadora {
+	
+	
+	/**
+	 * @param primeiroNumero
+	 * @param segundoNumero
+	 * @return retorna um inteiro, resultado da soma dos dois numeros inseridos
+	 */
+	public int somar(int primeiroNumero, int segundoNumero) {
+		return primeiroNumero+segundoNumero;
+	}
+	
+	//COMANDO PRA EXECUTAR DOCUMENTAÇÃO
+	//javadoc -d ../docs -sourcepath src -subpackages edu.yan.aulasintaxe.exemplodocumentacao
+}
---
+++ terminal-e-argumentos/src/edu/yan/aulasintaxe/conteudo/ClasseExecutavel.java
@@ -0,0 +1,7 @@
+package edu.yan.aulasintaxe.conteudo;
+
+public class ClasseExecutavel {
+	public static void main(String[] args) {
+		System.out.println("Executando teste");
+	}
+}
